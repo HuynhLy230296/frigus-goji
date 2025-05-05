63c98e63abe8a1e011dcc057a1c919ab497fc198
ly.huynh <huynhly230296@gmail.com>
2025-05-05 20:23:30 +0700

    fix: adjust UI center of header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -13,39 +13,41 @@ export default function Header() {
       style={{ height: 'var(--header-height)' }}
     >
       {/* Left */}
-      <div className=" w-[258px] rounded-full bg-gray-200">
-        <label
-          className="input input-sm input-ghost flex items-center justify-between"
-          aria-label="Tìm kiếm"
-          htmlFor="search"
-        >
-          <svg
-            className="mr-3 h-[1em] opacity-50"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
+      <div className="w-1/5">
+        <div className=" w-[258px] rounded-full bg-gray-200">
+          <label
+            className="input input-sm input-ghost flex items-center justify-between"
+            aria-label="Tìm kiếm"
+            htmlFor="search"
           >
-            <g
-              strokeLinejoin="round"
-              strokeLinecap="round"
-              strokeWidth="2.5"
-              fill="none"
-              stroke="currentColor"
+            <svg
+              className="mr-3 h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
             >
-              <circle cx="11" cy="11" r="8" />
-              <path d="m21 21-4.3-4.3" />
-            </g>
-          </svg>
-          <input
-            id="searcg"
-            type="search"
-            className=" grow"
-            placeholder="Tìm kiếm"
-          />
-        </label>
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <circle cx="11" cy="11" r="8" />
+                <path d="m21 21-4.3-4.3" />
+              </g>
+            </svg>
+            <input
+              id="searcg"
+              type="search"
+              className=" grow"
+              placeholder="Tìm kiếm"
+            />
+          </label>
+        </div>
       </div>
 
       {/* Center */}
-      <div className="flex items-center space-x-4">
+      <div className="flex flex-1 items-center justify-center space-x-4">
         <button
           type="button"
           className="btn btn-ghost"
@@ -66,16 +68,22 @@ export default function Header() {
 
       {/* Right */}
       {/* Profile */}
-      <div className="flex items-center space-x-2">
-        <button type="button" className="btn btn-ghost" aria-label="Tin nhắn">
-          <i className="fas fa-comment-dots  text-xl" />
-        </button>
-        <button type="button" className="btn btn-ghost" aria-label="Tài khoản">
-          <i className="fas fa-bell text-xl" />
-        </button>
-        <div className="avatar">
-          <div className="w-8 rounded-full">
-            <img alt="profile" src={user?.imageUrl} />
+      <div className="w-1/5">
+        <div className="flex items-center justify-end space-x-2">
+          <button type="button" className="btn btn-ghost" aria-label="Tin nhắn">
+            <i className="fas fa-comment-dots  text-xl" />
+          </button>
+          <button
+            type="button"
+            className="btn btn-ghost"
+            aria-label="Tài khoản"
+          >
+            <i className="fas fa-bell text-xl" />
+          </button>
+          <div className="avatar">
+            <div className="w-8 rounded-full">
+              <img alt="profile" src={user?.imageUrl} />
+            </div>
           </div>
         </div>
       </div>
